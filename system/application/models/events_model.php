6c761752bf2267510e6385e1c7de5eb3e01c3192
Mat Sadler <mat@redstudio.co.uk>
2011-09-09 15:44:09 +0100

    should be able to update event now

--- system/application/models/events_model.php
+++ system/application/models/events_model.php
@@ -17,6 +17,7 @@ class Events_model extends Model {
         function get_event($id)
 	{
 
+
                 $this->db->where('event_id', $id);
 		$query = $this->db->get('mydb_events');
 		if($query->num_rows > 0);
@@ -28,8 +29,30 @@ class Events_model extends Model {
 
         function update_event($id)
         {
+             //convert start date and time to single unix entry
+            $startdate = $this->input->post('startdate_unix');
+            $starttime = $this->input->post('starttime');
+            $unixstarttime = ($starttime * 60)*60;
+            $startdatetime = $startdate + $unixstarttime;
 
-            return;
+            //convert end date and time to single unix entry
+            $enddate = $this->input->post('enddate_unix');
+            $endtime = $this->input->post('endtime');
+            $unixendtime = ($endtime * 60)*60;
+            $enddatetime = $enddate + $unixendtime;
+
+
+
+            $update_event = array(
+					'location_title' => $this->input->post('location'),
+                                        'hosted_by_company' => $this->input->post('hosted_by_company'),
+                                        'startdate' => $startdatetime,
+                                        'enddate' => $enddatetime
+					);
+
+            $this->db->where('event_id', $id);
+           $update = $this->db->update('mydb_events', $update_event);
+            return $update;
         }
 
         function add_event()
