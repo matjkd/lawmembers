859c46cbeecb2e07ea9583520e83c983206e87aa
Mat Sadler <mat@redstudio.co.uk>
2011-09-09 19:17:22 +0100

    images should now display

--- system/application/views/events/view_gallery.php
+++ system/application/views/events/view_gallery.php
@@ -1,8 +1,8 @@
 <?php foreach($gallery_images as $row):?>
 
-
-<?=$row['image_filename']?>
-
+<div style="float:left; width:100px;"?>
+<img width="90px" src="https://s3-eu-west-1.amazonaws.com/laworld/events/<?=$row['image_folder']?>/<?=$row['image_filename']?>"/>
+</div>
 <?php endforeach ?>
 
 
