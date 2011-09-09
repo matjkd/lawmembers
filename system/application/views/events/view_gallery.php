06459d29f905a3c3d5e4bd9f222a06697df7fc83
Mat Sadler <mat@redstudio.co.uk>
2011-09-09 19:30:01 +0100

    stopped error when there are no images

--- system/application/views/events/view_gallery.php
+++ system/application/views/events/view_gallery.php
@@ -1,9 +1,12 @@
-<?php foreach($gallery_images as $row):?>
+<br/>
+<br/>
+<?php if(isset($gallery_images)){ foreach($gallery_images as $row):?>
 
 <div style="float:left; width:100px;"?>
-<img width="90px" src="https://s3-eu-west-1.amazonaws.com/laworld/events/<?=$row['image_folder']?>/<?=$row['image_filename']?>"/>
+<img width="90px" src="https://s3-eu-west-1.amazonaws.com/laworld/events/<?=$row['image_folder']?>/<?=$row['image_filename']?>"/><br/>
+ <a href="<?=base_url?>events/delete_image/<?=$row['image_id']?>">Delete</a>
 </div>
-<?php endforeach ?>
+<?php endforeach; } ?>
 
 
 
