b664e71d033015836aa84385cdba2ba233ce81d3
Mat Sadler <mat@redstudio.co.uk>
2011-09-09 22:31:24 +0100

    typo fix

--- system/application/views/frontend/view_event.php
+++ system/application/views/frontend/view_event.php
@@ -30,4 +30,12 @@ Location  <?=$row->location_title?>
 </div>
 <?php endforeach ?>
 
-    <?=$this->load->view('events/view_gallery')?>
+<br/>
+<br/>
+<?php if(isset($gallery_images)){ foreach($gallery_images as $row):?>
+
+<div style="float:left; width:100px;"?>
+<img width="90px" src="https://s3-eu-west-1.amazonaws.com/laworld/events/<?=$row['image_folder']?>/<?=$row['image_filename']?>"/><br/>
+ 
+</div>
+<?php endforeach; } ?>
