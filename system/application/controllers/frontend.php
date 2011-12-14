8400a68c23b1ffc269ec17fecc0aa5d36d70d7cd
Mat Sadler <mat@redstudio.co.uk>
2011-12-14 14:35:57 +0000

    added sidebox controller for events

--- system/application/controllers/frontend.php
+++ system/application/controllers/frontend.php
@@ -1,51 +1,57 @@
 <?php
-class Frontend extends MY_Controller
-{
-function __construct()
-	{
-		parent::__construct();
-		$this->load->model('companies_model');
-                $this->load->model('users_model');
-                $this->load->model('events_model');
-                $this->load->model('gallery_model');
 
-		$this->load->library('s3');
+class Frontend extends MY_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->model('companies_model');
+        $this->load->model('users_model');
+        $this->load->model('events_model');
+        $this->load->model('gallery_model');
+
+        $this->load->library('s3');
+    }
+
+    function index() {
+        redirect('events/view/');
+    }
+
+    function view_events() {
+        $data['events'] = $this->events_model->get_events();
+
+        $data['main'] = "frontend/view_events";
+        $this->load->vars($data);
+
+        $this->load->view('frontend/template');
+    }
+     function view_sideevents() {
+        $data['events'] = $this->events_model->get_events();
+
+        $data['main'] = "frontend/events_side";
+        $this->load->vars($data);
+
+        $this->load->view('frontend/template');
+    }
+
+    function view_event($id) {
+        $data['event'] = $this->events_model->get_event($id);
+        $data['main'] = "frontend/view_event";
+        $data['folder_info'] = $this->gallery_model->get_eventgallery($id);
 
 
-	}
-	function index()
-	{
-		redirect('events/view/');
-	}
-        function view_events()
-        {
-               $data['events'] = $this->events_model->get_events();
-               
-               $data['main'] = "frontend/view_events";
-               $this->load->vars($data);
+        //grab some variables for the folder
+        foreach ($data['folder_info'] as $row):
+            $data['folder_name'] = $row['folder_name'];
+            $data['folder_id'] = $id;
+            $data['safe_name'] = $row['safe_name'];
+            $data['account_id'] = 'events';
+        endforeach;
+        $image_folder = $data['safe_name'];
 
-		$this->load->view('frontend/template');
-        }
+        $data['gallery_images'] = $this->gallery_model->get_images($image_folder);
+        $this->load->vars($data);
 
-        function view_event($id)
-        {
-             $data['event'] = $this->events_model->get_event($id);
-                 $data['main'] = "frontend/view_event";
-                   $data['folder_info'] = $this->gallery_model->get_eventgallery($id);
-           
+        $this->load->view('frontend/template');
+    }
 
-	//grab some variables for the folder
-	foreach($data['folder_info'] as $row):
-		$data['folder_name'] = $row['folder_name'];
-		$data['folder_id'] = $id;
-		$data['safe_name'] = $row['safe_name'];
-		$data['account_id'] = 'events';
-	endforeach;
-        $image_folder= $data['safe_name'];
-
-             $data['gallery_images'] = $this->gallery_model->get_images($image_folder);
-               $this->load->vars($data);
-
-		$this->load->view('frontend/template');
-        }
 }
