cb8d8e38c4e1661812b15f0f5be8f6586bd9a12a
Mat Sadler <mat@redstudio.co.uk>
2012-05-04 09:20:51 +0000

    changed first page of newsletters

---
+++ system/application/views/frontend/newsletters/intro.php
@@ -0,0 +1,46 @@
+<div id="selectform">
+	<?= form_open('frontend/view_newsletters') ?>
+	<select name="country">
+		<option value="">Please Select</option>
+		<?php foreach ($countries as $row): 
+
+		if($row->country == $country) {
+			$selected ="selected='selected'";
+		} else {
+			$selected ="";
+		}
+
+		?>
+
+
+		<option <?=$selected?> value="<?= $row->country ?>">
+			<?= $row->country ?>
+		</option>
+		<?php endforeach;
+		?>
+	</select>
+	<?= form_submit('Submit', 'Select Country') ?>
+
+	<?= form_close() ?>
+
+</div>
+
+<?php foreach ($countries as $row): ?>
+
+<?= form_open('frontend/view_newsletters') ?>
+<div style="width:100%; height:30px; padding:10px 0; margin-bottom:20px; background:#ddd;">
+	<?= form_hidden('country', $row->country) ?>
+	<div style="color:#00478F; float:left; padding-top:8px; padding-left:20px;">
+		<h4><?= $row->country ?></h4>
+	</div>
+	<div style="float:right; padding-right:20px;">
+		<?= form_submit('Submit', 'Select Country') ?>
+	</div>
+
+
+</div>
+<div style="clear:both"></div>
+<?= form_close() ?>
+
+
+<?php endforeach; ?>
--- system/application/views/frontend/newsletters/main.php
+++ system/application/views/frontend/newsletters/main.php
@@ -1,67 +1,75 @@
 <div id="selectform">
-<?= form_open('frontend/view_newsletters') ?>
-<select name="country">
-     <option  value="">Please Select</option>
-    <?php foreach ($countries as $row): 
-        
-        if($row->country == $country) {
-            $selected ="selected='selected'";
-        } else {
-            $selected ="";
-        }
-        
-        ?>
-    
-    
-        <option <?=$selected?> value="<?= $row->country ?>"><?= $row->country ?></option>
-    <?php endforeach;
-    ?>
-</select>
-<?= form_submit('Submit', 'Select Country') ?>
+	<?= form_open('frontend/view_newsletters') ?>
+	<select name="country">
+		<option value="">Please Select</option>
+		<?php foreach ($countries as $row): 
 
-<?= form_close() ?>
+		if($row->country == $country) {
+			$selected ="selected='selected'";
+		} else {
+			$selected ="";
+		}
+
+		?>
+
+
+		<option <?=$selected?> value="<?= $row->country ?>">
+			<?= $row->country ?>
+		</option>
+		<?php endforeach;
+		?>
+	</select>
+	<?= form_submit('Submit', 'Select Country') ?>
+
+	<?= form_close() ?>
 
 </div>
-<script src="<?= base_url() ?>js/tables/tables.js" type="text/javascript" ></script>
-<table id="newsletter_table" style="width:100%;">
-    <thead>
-        <tr>
-            <th>Date</th>
-            <th>Title</th>
-            <th>Jurisdiction</th>
-      
-
-
-        </tr>
-
-    </thead>
-    <tbody>
-        <?php
-        foreach ($newsletters as $row):
-
-            $link = str_replace(" ", "+", $row->newsletter_filename);
-            $datestring1 = "%F %Y ";
-            $datestring2 = " %d/%m/%Y ";
-            $time = $row->newsletter_date;
-            $time2 = $row->date_added;
-            $newsletter_date = mdate($datestring1, $time);
-            $date_added = mdate($datestring2, $time2);
-            ?>
-
-            <tr>
-           
-                <td><span style="display:none;"><?=$time?></span><?=$newsletter_date?></td>
-                     <td>  <a href="https://s3-eu-west-1.amazonaws.com/laworldnewsletters/<?= $link ?>"><?= $row->newsletter_title ?></a><br/> <em>Written by <?= $row->company_name ?></em> </td>
-               
-                <td> <?= $row->city ?>, <?= $row->country ?></td>
-               
-
-
-            </tr>
+<script
+	src="<?= base_url() ?>js/tables/tables.js" type="text/javascript"></script>
+<table id="newsletter_table" style="width: 100%;">
+	<thead>
+		<tr>
+			<th>Date</th>
+			<th>Title</th>
+			<th>Jurisdiction</th>
 
 
 
+		</tr>
 
-        <?php endforeach; ?>
-    </tbody>
+	</thead>
+	<tbody>
+		<?php
+		foreach ($newsletters as $row):
+
+		$link = str_replace(" ", "+", $row->newsletter_filename);
+		$datestring1 = "%F %Y ";
+		$datestring2 = " %d/%m/%Y ";
+		$time = $row->newsletter_date;
+		$time2 = $row->date_added;
+		$newsletter_date = mdate($datestring1, $time);
+		$date_added = mdate($datestring2, $time2);
+		?>
+
+		<tr>
+
+			<td><span style="display: none;"><?=$time?> </span> <?=$newsletter_date?>
+			</td>
+			<td><a
+				href="https://s3-eu-west-1.amazonaws.com/laworldnewsletters/<?= $link ?>"><?= $row->newsletter_title ?>
+			</a><br /> <em>Written by <?= $row->company_name ?>
+			</em>
+			</td>
+
+			<td><?= $row->city ?>, <?= $row->country ?></td>
+
+
+
+		</tr>
+
+
+
+
+		<?php endforeach; ?>
+	</tbody>
 </table>
