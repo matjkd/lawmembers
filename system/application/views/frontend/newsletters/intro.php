2ef7eec50981ac80f6904856b91467bb28310ccc
Mat Sadler <mat@redstudio.co.uk>
2012-05-04 09:26:39 +0000

    tweaked css some more

--- system/application/views/frontend/newsletters/intro.php
+++ system/application/views/frontend/newsletters/intro.php
@@ -28,10 +28,10 @@
 <?php foreach ($countries as $row): ?>
 
 <?= form_open('frontend/view_newsletters') ?>
-<div style="width:100%; height:30px; padding:3px 0; margin-bottom:10px; background:#ddd;">
+<div style="width:100%; height:30px; padding:3px 0; margin-bottom:0px; margin-top:3px; background:#ddd;">
 	<?= form_hidden('country', $row->country) ?>
-	<div style="color:#00478F; float:left; padding-top:8px; padding-left:20px;">
-		<h4><?= $row->country ?></h4>
+	<div style="color:#00478F; font-size:1.3em; font-weight:bold; float:left; padding-top:7px; padding-left:20px;">
+		<?= $row->country ?>
 	</div>
 	<div style="float:right; padding-right:20px;">
 		<?= form_submit('Submit', 'Select Country') ?>
